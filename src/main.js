411d0bd7a08f3cc0ee0316ccbf9c749d769a56df
Ian Hwang <ian807b@gmail.com>
2025-02-16 22:02:19 -0800

    JS-Files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -12,4 +14,23 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
+
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
 });

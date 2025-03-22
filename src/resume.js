a43049f12202b9608f9a00f09591226e8eddf414
Ian Hwang <ian807b@gmail.com>
2025-03-21 20:31:26 -0700

    Updated projects

--- src/resume.js
+++ src/resume.js
@@ -1,4 +1,4 @@
-const driveFileId = '1WeuQ8emhX49AvorTTzXijSDDVlX7lp49';
+const driveFileId = '1NZY01mhh4TVuuWbfTU3fL42sFp5Y6jD6';
 const resumeFile = `https://drive.google.com/file/d/${driveFileId}/preview`;
 
 document.querySelector('.resume-link').addEventListener('click', (event) => {
